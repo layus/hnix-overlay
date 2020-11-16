181dc84a9543ffa116c5cb15f8fa4ed6c0ef72c3
Guillaume Maudoux <layus.on@gmail.com>
2020-11-16 11:26:44 +0100

    add the updated hnix-store as dependency

--- nix/hnix-store.nix
+++ nix/hnix-store.nix
@@ -2,10 +2,10 @@ self: super:
 let
   # https://github.com/haskell-nix/hnix-store
   src = super.fetchFromGitHub {
-    owner = "haskell-nix";
+    owner = "layus";
     repo = "hnix-store";
-    rev = "2497d37d35eeed854875e9245c02bf538eaafa10";
-    sha256 = "1xkj99ba4rc9mgd14bjsk170qvhmhrx00zz5bwml4737h3izl84k";
+    rev = "lift";
+    sha256 = "00jx8cn9j709sycvnp3jmk1hbc8jmpjd8r6d0d9b0rwfzhva6lzv";
   };
 
   # swap with src to build from this path
